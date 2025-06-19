b5c9accfb046522df69c5e7cc0a59d4cc3cb42f4
Choi_ji_an <96537605+Ji-ny@users.noreply.github.com>
2025-06-19 17:14:32 +0900

    [level 3] Title: 단어 변환, Time: 6.68 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=python3) 
 
 ### 성능 요약
 
-메모리: 36.5 MB, 시간: 5.07 ms
+메모리: 9.07 MB, 시간: 6.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 18일 20:26:13
+2025년 06월 19일 17:14:29
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -3,33 +3,44 @@ from collections import deque
 def solution(begin, target, words):
     answer = 0
     
-    # target이 words 내부에 없는 경우는 0을 리턴
+    # 1. 변환할 수 없는 경우는 0 return
+    # -> 즉, words 안에 target이 없다면 0 리턴
     if target not in words:
-        return 0
-    
+        return 0    
+    # bfs를 이용해서 step을 기록해서 
+    # 1개만 다른 단어를 찾으면 queue에 넣고 변환과정을 넣는다
+    # 이게참… 신기하단말이지 무한반복될줄 알았는데
     return bfs(begin, target, words)
 
-# 최소 단계를 찾아야하므로 BFS
 def bfs(begin, target, words):
     queue = deque()
-    queue.append([begin, 0]) # 시작 단어와 단계 0으로 초기화
+    queue.append([begin, 0]) # 큐 선언
     
+    
+    # 큐에 변환 가능한 단어가 있을때까지 무한반복
     while queue:
+        # print(queue)
+        # 단어와 현재 단계 저장
+        now, step = queue.popleft() # 값 저장 (현재 단어, step)
         
-        now, step = queue.popleft()
-        # print(now, step)
+        # 만약 현재 찾은 단어가 목표 단어라면, 반복 마무리 후 리턴
         if now == target:
             return step
+
         
-        # 단어를 모두 체크하면서, 해당 단어가 변경될 수 있는지 체크
+        # words 배열을 순회하면서 단어 바꿔가면서 진행하기 
         for word in words:
-            # print('word', word)
-            count = 0
-            for i in range(len(now)): # 단어의 길이만큼 반복하여
-                if now[i] != word[i]: # 단어에 알파벳 한개씩 체크
-                    count += 1 # 같지 않은 부분만 추가
+            count = 0 # 같지 않은 단어 자리 개수 
+            for i in range(len(word)):
+                # 단어가 1개만 다르다면 그 단어 추가 
+                if now[i] != word[i]:
+                    count += 1
             
-            if count == 1: # 바꿀 수 있는 단어 나오면 큐에 값 추가
-                queue.append([word, step+1])
-                # print(queue)
+            # 만약 같지 않은 단어가 한개라면, (즉 나머지가 모두 일치하다면, 한 자리 단어만 바꿀수있음)
+            if count == 1:
+                # 큐에 바꾸게 될 값 추가
+                queue.append([word, step+1]) # 그리고 다음 변환 단계로 진행
+            
+       
+    return 0
     
