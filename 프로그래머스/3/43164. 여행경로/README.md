5be2825af4db73a2c9341351d6f6330c3a3d6e6f
Choi_ji_an <96537605+Ji-ny@users.noreply.github.com>
2025-06-21 16:49:56 +0900

    [level 3] Title: 여행경로, Time: 471.56 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=python3) 
 
 ### 성능 요약
 
-메모리: 60.5 MB, 시간: 267.67 ms
+메모리: 13.5 MB, 시간: 471.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 21일 16:29:08
+2025년 06월 21일 16:49:55
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,20 +1,35 @@
+# 항상 ICN 공항에서 출발한다
+# tickets 배열을 순회하며, 공항 경로를 DFS로 탐색한다
+# ticket 은 [출발-> 도착] 이므로, 현재 위치와 티켓의 출발지가 같다면 dfs로 다음 경로로 다시 탐색하게 한다
+# dfs(start, end)
+
 def solution(tickets):
     answer = []
+    # 티켓의 방문 여부 배열 저장
     visited = [False] * len(tickets)
     
+    # dfs 탐색 로직 작성
     def dfs(start, path):
-        if (len(path) == len(tickets) + 1): # 현재까지 이동한 경로가 모든 티켓을 다 사용한 경로라면, 결과에 path 저장 
-            answer.append(path)
-            return # 마무리 
+        # 현재 모두 방문했다면? 
+        if len(path) == len(tickets)+1:
+            answer.append(path) # 경로 저장
+            return # 현재 DFS 마무리
         
-        for idx, ticket in enumerate(tickets):
-            if (ticket[0] == start) and (not visited[idx]): # 출발지랑 다음 위치 start 지점이 같다면 , 그리고 아직 방문 안한곳이라면
-                visited[idx] = True
-                dfs(ticket[1], path + [ticket[1]]) # 도착지로 저장 및 도착지로 이동 
-                visited[idx] = False # 현재 위치 다시 안간걸로 저장  (dfs 끝까지 한 다음에 다른 경로 탐색해야함 )
-    
-    dfs("ICN", ["ICN"]) # 출발지와 경로
-    
-    answer.sort() # 최종적으로 도출된 정딥라스트 값을 알파벳 순서로 정렬할거
-    
-    return answer[0] # 제일앞 경로 출력
+        # 티켓을 순회하며 갈 수 있는 항공편 탐색 
+        for index, ticket in enumerate(tickets): 
+            # 현재 목표 출발지와 티켓의 출발지가 같다면? && 아직 사용 안한 티켓이라면?
+            if (start == ticket[0] and not visited[index]):
+                # 현재 위치 방문 처리
+                visited[index] = True
+                
+                # 현재 항공편으로 간뒤 다음 경로 탐색
+                dfs(ticket[1], path + [ticket[1]])
+                
+                # dfs 끝나면 다시 다른 경로로 탐색해야하므로 방문처리 해제
+                visited[index] = False
+        
+        
+    dfs("ICN", ["ICN"]) ## 출발지, 현재 저장된 경로
+    answer.sort()
+    # print(answer)
+    return answer[0]
