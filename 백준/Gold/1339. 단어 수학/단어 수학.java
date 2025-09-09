6f31bc48c0d4acb4c3073a5c342a7e86c85dac75
Choi_ji_an <96537605+Ji-ny@users.noreply.github.com>
2025-09-09 17:51:27 +0900

    [Gold IV] Title: 단어 수학, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -2,52 +2,47 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        // 단어의 개수 N
-        int N = Integer.parseInt(st.nextToken());
-        Integer[] array = new Integer[26]; // 알파벳 개수 
-        Arrays.fill(array, 0);
 
-        // N개의 줄에 단어가 한줄에 하나씩 주어진다. (String 배열 )
+        int N = Integer.parseInt(st.nextToken());
+
+        // 1. 알파벳 개수만큼 배열 선언
+        Integer[] alphabet = new Integer[26];
+        Arrays.fill(alphabet, 0);
+
+        // 2. 단어의 개수만큼 입력
         for (int i = 0; i < N; i++) {
-            // st = new StringTokenizer(br.readLine());
-            String str = br.readLine();
-            // str의 길이만큼 가중치를 구한다.
+            st = new StringTokenizer(br.readLine());
+
+            String str = st.nextToken();
+            // 알파벳을 순회하며 알파벳 위치에 값 추가
             for (int j = 0; j < str.length(); j++) {
-                char c = str.charAt(j);
-                // 구한 가중치를 배열에 넣는다.
-                // 1의 자리면 1, 100의 자리면 100을 만들어줘야함 
-                array[c - 'A'] += (int) Math.pow(10, str.length() - 1 - j); // 지수 함수 
+                int index = str.charAt(j) - 'A';
+                alphabet[index] += (int) Math.pow(10, str.length() - j - 1);
             }
         }
 
-        // 2. 큰값기준이니 내림차순 정렬 
-        Arrays.sort(array, (a,b) -> b - a);
+        // 알파벳 배열 sort
+        Arrays.sort(alphabet, (a, b) -> b - a);
 
-        // * 3. 9부터 1씩 빼면서 높은 수 넣기 (2까지)
-        // * 25번 (알파벳 개수만큼 돈다.)
+        // 9부터 시작해서 값 넣어주기
         int number = 9;
         int answer = 0;
-        for (int i = 0; i < 26; i++) {
-            if (array[i] == 0) { // 0이 들어온거면 이제 더할거 없으니 종료
+        // 큰수부터 반복 진행
+        for (int j = 0; j < alphabet.length; j++) {
+            if (alphabet[j] == 0) {
                 break;
             }
 
-            answer += array[i] * number; // 결과에 현재 가중치 * 숫자
-            number--; // 9, 8, 7... 이렇게 값을 넣어줄것 
-
+            answer += alphabet[j] * number; // *number만큼 큰수부터 추가
+            number--;
         }
 
         System.out.println(answer);
-        // System.out.println(Arrays.toString(array));
-
-
-        
     }
 }
