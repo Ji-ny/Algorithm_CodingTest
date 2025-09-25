f07ab147dd8cc67546b3ee1377be3cc62bc1dcb4
Choi_ji_an <96537605+Ji-ny@users.noreply.github.com>
2025-09-25 22:35:33 +0900

    [level 2] Title: 하노이의 탑, Time: 4.77 ms, Memory: 48.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.js
@@ -0,0 +1,18 @@
+function solution(n) {
+    var answer = [];
+    
+    const hanoi = (src, tgt, inter, n) => {
+        if (n == 1) {
+            answer.push([src, tgt]);
+        } else {
+            hanoi(src, inter, tgt, n-1);
+            hanoi(src, tgt, inter, 1);
+            hanoi(inter, tgt, src, n-1);
+        }
+    } 
+    
+    
+    hanoi(1, 3, 2, n);
+    
+    return answer;
+}
